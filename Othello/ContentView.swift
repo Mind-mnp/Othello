747abf2f2ab558f06bf549ad523b77219737ad4f
NICHAPA <nichapa.por@dome.tu.ac.th>
2023-11-29 14:55:50 +0700

    create board game
    info

---
+++ Othello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Othello
+//
+//  Created by Mind Nichapa on 23/11/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Othello/OthelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  OthelloApp.swift
+//  Othello
+//
+//  Created by Mind Nichapa on 23/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct OthelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GaneView()
+        }
+    }
+}
