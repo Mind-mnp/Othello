867fe97d10d4c4a89c603edbb10d5aa9c6b11fd2
ワルンポ-ンさん <tankubopa77@gmail.com>
2023-12-15 19:31:33 +0700

    update

--- Othello/GaneView.swift
+++ Othello/GaneView.swift
@@ -7,80 +7,39 @@
 
 import SwiftUI
 
-enum Player: Int {
-    case none
-    case black
-    case white
-    
-    var color: Color {
-        switch self {
-        case .black:
-            return .black
-        case .white:
-            return .white
-        case .none:
-            return .clear
-        }
-    }
-}
-
 struct GaneView: View {
-    
-    
-    
-    @State private var currentTurn: Player = .black
-    
-    
-    // Define valueBoard as a 2D array of Color
-    //     @State private var valueBoard: [[Color]] = {
-    //         var board = Array(repeating: Array(repeating: Color.clear, count: 6), count: 6)
-    //         board[2][2] = .black
-    //         board[3][3] = .black
-    //         board[2][3] = .white
-    //         board[3][2] = .white
-    //         return board
-    //     }()
-    @State var valueBoard : [[Color]] = Array(repeating: Array(repeating: .clear, count: 6),count: 6)
-    @State var countBlack = 2
-    @State var countWhite = 2
-    
-    
-    
-    
-    
+    @StateObject private var gameLogic = GameLogic()
+    @State private var showWinnerPopup = false
+    @State private var winnerMessage = ""
+
     var body: some View {
         let primary_color = Color.black
-        
-        
-        //header
-        Text("🎴OTHELLO")
-            .font(.system(size: 55))
-            .padding(.horizontal).bold()
-            .foregroundColor(primary_color)
-        HStack{
-            Text("Turn")
-                .font(.system(size: 30))
-                .padding(.horizontal)
-                .foregroundColor(primary_color)
-            Chip(color: currentTurn.color)
-            
-        }
-        
-        //Board
+
         VStack {
+            // Header
+            Text("🎴OTHELLO")
+                .font(.system(size: 55))
+                .padding(.horizontal).bold()
+                .foregroundColor(primary_color)
+
+            HStack {
+                Text("Turn")
+                    .font(.system(size: 30))
+                    .padding(.horizontal)
+                    .foregroundColor(primary_color)
+
+                Chip(color: gameLogic.currentTurn.color)
+            }
+
+            // Board
             VStack(spacing: 1) {
                 ForEach(0..<6, id: \.self) { row in
                     HStack(spacing: 1) {
                         ForEach(0..<6, id: \.self) { column in
-                            CardView(value: valueBoard[row][column])
-                                .foregroundColor(primary_color) // Use primary_color here
+                            CardView(value: gameLogic.valueBoard[row][column])
+                                .foregroundColor(primary_color)
                                 .onTapGesture {
-                                    // Check if the cell is clear and the move is valid
-                                    if valueBoard[row][column] == .clear && isValidMove(row: row, column: column, player: currentTurn) {
-                                        makeMove(row: row, column: column, player: currentTurn)
-                                        currentTurn = (currentTurn == .black) ? .white : .black
-                                        updateCounts()
-                                    }
+                                    gameLogic.handleTap(row: row, column: column)
                                 }
                         }
                     }
@@ -89,190 +48,62 @@ struct GaneView: View {
             .padding()
             .background(Color.black)
             .padding()
-        }
-        .onAppear {
-            // Set initial values for valueBoard when the view appears
-            setupInitialBoard()
-        }
-        
-        
-        // Check if the game has ended and display the result
-        if isGameEnded() {
-                    Text(determineWinner())
-                        .font(.system(size: 40, weight: .bold, design: .rounded)) // Bold, rounded font
-                        .foregroundColor(.white) // White text color for better contrast
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 20)
-                        .background(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.8), Color.green.opacity(0.8)]), startPoint: .topLeading, endPoint: .bottomTrailing)) // Gradient background
-                        .cornerRadius(15) // Rounded corners
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 15)
-                                .stroke(Color.white, lineWidth: 2) // White border
-                        )
-                        .shadow(radius: 10) // Shadow for a 3D effect
-                        .padding()
-                        .transition(.move(edge: .top).combined(with: .opacity)) // Transition effect
-                        .animation(.easeInOut(duration: 1.0), value: isGameEnded()) // Updated animation syntax
-                }
 
+            if gameLogic.isGameEnded() {
+                ResultPopup(message: gameLogic.determineWinner(), onRestart: 
+                                gameLogic.setupInitialBoard)
+                    .transition(.scale)
+                    .onAppear {
+                        winnerMessage = gameLogic.determineWinner()
+                        showWinnerPopup = true
+                    }
+            }
 
+            // Footer
+            HStack {
+                // White Score
+                VStack {
+                    HStack {
+                        Text("White")
+                            .font(.system(size: 30))
+                            .padding(.horizontal)
+                            .foregroundColor(primary_color)
+                        Chip(color: .white)
+                    }
 
-        
-        //footer
-        HStack{
-            VStack{
-                HStack{
-                    Text("White")
+                    Text(String(gameLogic.countWhite))
                         .font(.system(size: 30))
                         .padding(.horizontal)
                         .foregroundColor(primary_color)
-                    Chip(color: .white)
                 }
-                
-                Text(String(countWhite))
-                    .font(.system(size: 30))
-                    .padding(.horizontal)
-                    .foregroundColor(primary_color)
-            }
-            
-            Spacer()
-            
-            VStack{
-                HStack{
-                    Text("Black")
+
+                Spacer()
+
+                // Black Score
+                VStack {
+                    HStack {
+                        Text("Black")
+                            .font(.system(size: 30))
+                            .foregroundColor(primary_color)
+                        Chip(color: .black)
+                    }
+
+                    Text(String(gameLogic.countBlack))
                         .font(.system(size: 30))
+                        .padding(.horizontal)
                         .foregroundColor(primary_color)
-                    Chip(color: .black)
-                }
-                
-                Text(String(countBlack))
-                    .font(.system(size: 30))
-                    .padding(.horizontal)
-                    .foregroundColor(primary_color)
-            }.padding()
-        }
-        
-    }
-    func setupInitialBoard() {
-            // Set the initial configuration for the board
-            valueBoard[2][2] = .white
-            valueBoard[3][3] = .white
-            valueBoard[2][3] = .black
-            valueBoard[3][2] = .black
-        }
-
-    func handleTap(row: Int, column: Int) {
-        // Only proceed if the move is valid
-        if isValidMove(row: row, column: column, player: currentTurn) {
-            makeMove(row: row, column: column, player: currentTurn)
-            currentTurn = currentTurn == .black ? .white : .black
-            updateCounts()
-        }
-    }
-
-
-
-        func isValidMove(row: Int, column: Int, player: Player) -> Bool {
-        guard valueBoard[row][column] == .clear else { return false }
-
-        // Check all eight directions
-        let directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        for (dx, dy) in directions {
-            var x = row + dx
-            var y = column + dy
-            var foundOpponent = false
-
-            while x >= 0 && x < 6 && y >= 0 && y < 6 && valueBoard[x][y] != .clear {
-                if valueBoard[x][y] != player.color {
-                    foundOpponent = true
-                    x += dx
-                    y += dy
-                } else {
-                    if foundOpponent {
-                        return true
-                    }
-                    break
-                }
+                }.padding()
             }
         }
-
-        return false
-    }
-
-
-    func makeMove(row: Int, column: Int, player: Player) {
-        let directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        valueBoard[row][column] = player.color
-
-        for (dx, dy) in directions {
-            var x = row + dx
-            var y = column + dy
-            var path: [(Int, Int)] = []
-
-            while x >= 0 && x < 6 && y >= 0 && y < 6 && valueBoard[x][y] != .clear {
-                if valueBoard[x][y] != player.color {
-                    path.append((x, y))
-                    x += dx
-                    y += dy
-                } else {
-                    if !path.isEmpty {
-                        for (px, py) in path {
-                            valueBoard[px][py] = player.color
-                        }
-                    }
-                    break
-                }
-            }
+        .onAppear {
+            gameLogic.setupInitialBoard()
         }
     }
-
-
-    func updateCounts() {
-        countBlack = valueBoard.flatMap { $0 }.filter { $0 == .black }.count
-        countWhite = valueBoard.flatMap { $0 }.filter { $0 == .white }.count
-    }
-    
-    func isGameEnded() -> Bool {
-        // Check if the board is full
-        let isBoardFull = !valueBoard.flatMap { $0 }.contains(.clear)
-        
-        // Check if neither player has a valid move
-        let noValidMovesForBlack = !valueBoard.indices.contains(where: { row in
-            valueBoard[row].indices.contains(where: { column in
-                isValidMove(row: row, column: column, player: .black)
-            })
-        })
-
-        let noValidMovesForWhite = !valueBoard.indices.contains(where: { row in
-            valueBoard[row].indices.contains(where: { column in
-                isValidMove(row: row, column: column, player: .white)
-            })
-        })
-
-        return isBoardFull || (noValidMovesForBlack && noValidMovesForWhite)
-    }
-    
-    func determineWinner() -> String {
-        if countBlack > countWhite {
-            return "Black wins!"
-        } else if countWhite > countBlack {
-            return "White wins!"
-        } else {
-            return "It's a tie!"
-        }
-    }
-
-
-
-
-    
 }
-
-
 
 struct Chip: View {
     let color: Color
-    
+
     var body: some View {
         Circle()
             .frame(width: 50, height: 50)
@@ -282,13 +113,9 @@ struct Chip: View {
     }
 }
 
-
-
-
 struct CardView: View {
-    //let content: String
     var value: Color
-    
+
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 12)
@@ -297,51 +124,40 @@ struct CardView: View {
             Circle()
                 .frame(width: 60, height: 60)
                 .foregroundColor(value)
-            
         }
-        
-        
     }
 }
 
 struct ResultPopup: View {
     let message: String
-    var onClose: () -> Void
+    let onRestart: () -> Void
 
     var body: some View {
         VStack(spacing: 20) {
             Text(message)
                 .font(.system(size: 40, weight: .bold, design: .rounded))
                 .foregroundColor(.white)
-                .padding(.vertical, 10)
-                .padding(.horizontal, 20)
-                .background(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.8), Color.green.opacity(0.8)]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                .padding()
+                .background(Color.blue)
                 .cornerRadius(15)
-                .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.white, lineWidth: 2))
-                .shadow(radius: 10)
 
-            Button(action: onClose) {
-                Text("Close")
-                    .font(.title)
-                    .bold()
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.red)
-                    .cornerRadius(10)
-                    .shadow(radius: 5)
-            }
+            Button("Play Again", action: onRestart)
+                .font(.system(size: 30))
+                .padding()
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(15)
         }
         .padding()
-        .background(Color.white.opacity(0.9))
+        .background(Color.white)
         .cornerRadius(20)
         .shadow(radius: 20)
-        .padding(40)
     }
 }
 
-
-
-
-#Preview {
-    GaneView()
+// Preview
+struct GaneView_Previews: PreviewProvider {
+    static var previews: some View {
+        GaneView()
+    }
 }
